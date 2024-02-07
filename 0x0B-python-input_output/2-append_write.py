fbe794a039fa52fb2f2e29e923ebfd208c2813f3
Isah Abdulsalam <isahabdulsalam416@gmail.com>
2024-02-06 17:09:56 -0800

    function that reads a text file (UTF8) and prints it to stdout:

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -5,4 +5,4 @@ function for append_write
 def append_write(filename="", text=""):
     """ defining append_write """
     with open(filename, 'a') as file:
-        return file.write()
+        return file.write(text)
