8c9f7ad1fc25e131cc822215270592281778f400
Isah Abdulsalam <isahabdulsalam416@gmail.com>
2024-03-13 15:55:29 -0700

    more queries

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,13 +1,8 @@
 -- creates the database hbtn_0d_2 and the user user_0d_2.
 
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2
-
-DROP USER IF EXISTS 'user_0d_2'@'localhost'
-
--- Create the user if it doesn't exist
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-
--- Grant SELECT privilege to the user on the database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+DROP USER IF EXISTS 'user_0d_2'@'localhost';
+CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES
 
