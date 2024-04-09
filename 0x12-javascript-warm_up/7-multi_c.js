52b1e79c396d8da8f055099a51c870ac7070f431
Isah Abdulsalam <Isahabdulsalam416@gmail.com>
2024-04-08 17:33:17 -0700

    js

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const args = parseInt(process.argv[2]);
+
+if (args) {
+	for (let a = 0; a < args; ++a) {
+		console.log('C is fun');
+	}
+} else {
+	console.log('Missing number of occurrences');
+}
