0c48c8377978d4ce2b715db7052730f919c32ab3
Isah Abdulsalam <Isahabdulsalam416@gmail.com>
2024-04-08 17:36:41 -0700

    js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,14 +3,14 @@
 const size = parseInt(process.argv[2]);
 
 if (size) {
-  for (let i = 0; i < size; ++i) {
-    let j = 0;
+  for (let a = 0; a < size; ++a) {
+    let b = 0;
 
-    for (; j < size; ++j) {
+    for (; b < size; ++b) {
       process.stdout.write('X');
     }
 
-    if (j === size) {
+    if (b === size) {
       console.log('');
     }
   }
