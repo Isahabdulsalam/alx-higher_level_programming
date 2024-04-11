48a16c8708d9a95b5cdfa9517d6cada191f34d75
Isah Abdulsalam <Isahabdulsalam416@gmail.com>
2024-04-11 10:36:35 -0700

    done

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -2,11 +2,12 @@
 module.exports = class Rectangle {
   constructor (width, height) {
     this.width = width;
-    this.height = height
+    this.height = height;
   }
-  print() {
+
+  print( ) {
     for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
     }
   }
-}
+};
