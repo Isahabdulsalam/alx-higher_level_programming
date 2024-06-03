5fc5592aabeb2a0554d6623f5e03ce4a73eeae25
Isah Abdulsalam <isahabdulsalam416@gmail.com>
2024-06-03 10:11:41 +0000

    done

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $.ajax({
+    url: 'https://swapi-api.alx-tools.com/api/people/5/?format=json',
+    method: 'GET',
+    success: function(data) {
+      $("DIV#character").text(data.name);
+    },
+    error: function() {
+      $("DIV#character").text("Failed to fetch character data");
+    }
+  });
+});
