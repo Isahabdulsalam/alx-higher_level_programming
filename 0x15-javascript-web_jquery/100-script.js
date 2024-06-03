abd8ce5d30c966f69268bdcd3036ac0bc7a9e053
Isah Abdulsalam <isahabdulsalam416@gmail.com>
2024-06-03 10:30:22 +0000

    done

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,7 @@
+function updateHeaderTextColor() {
+  let headerElement = document.querySelector('header');
+  headerElement.style.color = '#FF0000';
+}
+document.addEventListener('DOMContentLoaded', function() {
+  updateHeaderTextColor();
+});
